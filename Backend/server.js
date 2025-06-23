a8515161df8b71fa3a2077b5dc7589903129ab3e
Brice <bricealibyilingiro@gmail.com>
2025-06-24 03:44:39 +0900

    Update CORS configuration

--- Backend/server.js
+++ Backend/server.js
@@ -18,7 +18,7 @@ connectDB();
 app.set("trust proxy", 1);
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
